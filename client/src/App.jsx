463587d9c387a64530e8a776bb9a368003b6b0a3
sripathman <nsripathman@gmail.com>
2024-01-25 23:35:18 +0000

    create pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Search from './pages/Search';
+import SignIn from './pages/signIn';
+import SignUp from './pages/signUp';
+import Projects from './pages/Projects';
 
-const App = () => {
+
+
+
+
+export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    
+   
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/search' element={<Search />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/sign-in' element = {<SignIn />}/>
+      <Route path='/sign-up' element = {<SignUp/>}/>
+      <Route path='Projects' element ={<Projects/>}/>
+
+      
+     
+      
+    </Routes>
+   
+  </BrowserRouter>
   )
 }
-
-export default App
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+  './pages/**/*.{html,js}',
+  './components/**/*.{html,js}'],
   theme: {
     extend: {},
   },
